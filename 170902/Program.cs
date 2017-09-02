0ef2d85fb2a9a5afbd3e8c0ac5eefdeed4e37833
min seok Choi <masamizero@gmail.com>
2017-09-02 14:10:53 +0900

    테스트용

---
+++ 170902/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+            Console.WriteLine("*****");
+
+
+
+
+			for (int j = 0; j <= 4; j++)
+			{
+
+			    for (int i = 4; i >= j;i--)
+			    {
+
+			        Console.Write("*");
+			    }
+			Console.WriteLine();
+			}
+
+
+		}
+    }
+}
